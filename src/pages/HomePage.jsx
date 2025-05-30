616822207113f31913a22a43887875e8a86cadcd
ArnabBiswas123 <iamarnabbiswas2001@gmail.com>
2025-05-30 20:46:40 +0530

    first commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -28,13 +28,13 @@ const handleBuyNow = async (product) => {
 
     const { sessionId } = await response.json();
 
-    // 2. Redirect to Stripe Checkout
+    
     const stripe = await stripePromise;
     const { error } = await stripe.redirectToCheckout({ sessionId });
 
     if (error) {
       console.error("Stripe redirect error:", error.message);
-      // Optionally, show this to the user
+
     }
   } catch (err) {
     console.error("Error in handleBuyNow:", err.message);
